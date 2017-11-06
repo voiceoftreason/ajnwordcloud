0b56177a415e80b75e6bb6700c22d18d12b76aea
Andy Newton <1163332+voiceoftreason@users.noreply.github.com>
2017-11-06 02:44:43 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
 setup(name='ajnwordcloud',
-      version='0.2.1',
+      version='0.2.2',
       description='simple wordcloud generator',
       url='http://github.com/voiceoftreason/ajnwordcloud',
       author='Andy Newton',
       author_email='voiceoftreason@me.com',
       license='MIT',
       packages=find_packages(),
-      install_requires=['numpy>=1.6.1', 'pillow'],
+      install_requires=['pillow', 'numpy>=1.6.1'],
       ext_modules=[Extension("ajnwordcloud.ii_search",
                            ["ajnwordcloud/ii_search.c"],include_dirs=['.'])],
       include_package_data=True,
