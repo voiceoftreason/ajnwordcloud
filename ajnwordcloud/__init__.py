5d6238b03a06e642e6ae42cd1543f869a2ca96eb
Andy Newton <1163332+voiceoftreason@users.noreply.github.com>
2017-11-06 02:44:23 +0000

    Add files via upload

--- ajnwordcloud/__init__.py
+++ ajnwordcloud/__init__.py
@@ -1,7 +1,7 @@
 from .core import wordcloud
-from .data import count_words, load_csv
+from .data import count_words, load_csv, load_txt
 from .colours import red, green, blue, yellow, orange, pink, brown, white, gray, multi
 from .fonts import font_xkcd, font_mouse_memoirs, font_chunkfive
-from .mask_images import mask_ellipse, mask_roundrect, mask_cloud, mask_thought, mask_speech, mask_star5, mask_star6, mask_xmas_tree, mask_usa 
+from .mask_images import mask_ellipse, mask_roundrect, mask_cloud, mask_thought, mask_speech, mask_star5, mask_star6, mask_xmas_tree, mask_usa, mask_heart, mask_alien 
            
-__version__ = '0.2.1'
+__version__ = '0.2.2'
